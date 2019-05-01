99bae8f6b836b0de9f1838232e71ca179fc498b8
Dhruvi-Patil <42148829+Dhruvi-Patil@users.noreply.github.com>
2019-05-01 19:03:09 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,19 +1,21 @@
 //SPDX-License-Identifier: Apache-2.0
 var cors = require('cors');
+var bodyParser=require("body-parser");
 var cert = require('./controller.js');
 
+var urlencodedParser=bodyParser.urlencoded({extended: false});
 module.exports = function(app){
 
   app.get('/get_cert/:id', cors(), function(req, res){
     cert.get_cert(req, res);
   });
-  app.post('/addNewCertificate', cors(), function(req, res){
+  app.post('/addNewCertificate', cors(), urlencodedParser, function(req, res){
     cert.addNewCertificate(req, res);
   });
   app.get('/get_all_cert', cors(), function(req, res){
     cert.get_all_cert(req, res);
   });
-  app.post('/transfer_cert/:certificate_id/transferName', cors(), function(req, res){
+  app.post('/transfer_cert/:certificate_id/:transferName', cors(), function(req, res){
     cert.transfer_cert(req, res);
   });
   app.get('/get_student/:id', cors(), function(req, res){
@@ -22,7 +24,7 @@ module.exports = function(app){
   app.post('/addNewStudent', cors(), function(req, res){
     cert.addNewStudent(req, res);
   });
-  app.get('/Login:id1/:id2', cors(), function(req, res){
+  app.get('/Login/:id1/:id2', cors(), function(req, res){
     cert.Login(req, res);
   });
 }
